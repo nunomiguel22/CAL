ed16786f3b1346e663b00a97b94d206c0cb6400e
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2019-02-21 09:57:17 +0000

    Update Change.cpp

--- EX1/src/Change.cpp
+++ EX1/src/Change.cpp
@@ -4,9 +4,22 @@
 
 #include "Change.h"
 
-string calcChange(int m, int numCoins, int *coinValues)
-{
-    return "";
+#include <iostream>
+
+string calcChange(int m, int numCoins, int *coinValues) {
+
+	if (m <= 0) return "";
+
+	for (int i = numCoins - 1; i >= 0; --i){
+		if (coinValues[i] <= m){
+			cout << coinValues[i] << endl;
+
+
+			return to_string(coinValues[i]) + ';' + calcChange(m - coinValues[i], numCoins, coinValues);
+		}
+	}
+
+	return "-";
 }
 
 
