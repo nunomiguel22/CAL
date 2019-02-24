e9324dff0127782fe94270027724cb8fd7f6452d
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2019-02-24 16:02:23 +0000

    B

--- EX1/src/Change.cpp
+++ EX1/src/Change.cpp
@@ -4,22 +4,76 @@
 
 #include "Change.h"
 
-#include <iostream>
 
-string calcChange(int m, int numCoins, int *coinValues) {
+int min_coins(int *coinValues, int ind, int value){
 
-	if (m <= 0) return "";
+	int coins = 0;
 
-	for (int i = numCoins - 1; i >= 0; --i){
-		if (coinValues[i] <= m){
-			cout << coinValues[i] << endl;
+	while (value > 0){
+		if (coinValues[ind] <= value){
 
-
-			return to_string(coinValues[i]) + ';' + calcChange(m - coinValues[i], numCoins, coinValues);
+			value -= coinValues[ind];
+			++coins;
 		}
+		else --ind;
 	}
-
-	return "-";
+	return coins;
 }
 
 
+string calcChange(int m, int numCoins, int *coinValues) {
+
+	if (!m)
+		return "";
+
+	if (coinValues[0] > m)
+		return "-";
+
+	//Specific amount
+	int best = 0;
+	int coin_amount = 0;
+
+	for (int i = 0; i < numCoins; ++i){
+		int coins = min_coins(coinValues, i, m);
+		if (coins < coin_amount || coin_amount == 0){
+			coin_amount = coins;
+			best = i;
+		}
+	}
+
+	//Parse string
+	int ind = best;
+	stringstream ss;
+	while (m > 0){
+		if (coinValues[ind] <= m){
+			m -= coinValues[ind];
+			ss << coinValues[ind] << ';';
+		}
+		else --ind;
+	}
+
+	return ss.str();
+}
+
+
+
+
+
+//Formulate table
+
+/*int best [m + 1];
+int coin_amount[m + 1];
+
+for (int i = 0; i < numCoins; ++i){
+	for (int j = 1; j <= m; ++j ){
+		int coins = min_coins(coinValues, i, j);
+		if (coins < coin_amount[j] || coin_amount[j] == 0){
+			coin_amount[j] = coins;
+			best[j] = i;
+		}
+	}
+}
+*/
+
+
+//int ind = best[m];
