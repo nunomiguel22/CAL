0bee16f567f230f0eb9f56f35d073f4a79eabb96
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2019-05-25 16:10:36 +0100

    updated menu

--- source/menu.h
+++ source/menu.h
@@ -4,12 +4,12 @@
 
 void readUsers(std::vector<User *> &users, Graph<idNode> &graph);
 void saveUsers(std::vector<User *> &users);
+void addDriver(std::vector<User *> &users);
+void removeDriver(std::vector<User *> &users);
+void generatePathFast(Graph<idNode> &graph, std::vector<User *> &users);
+void generatePathBest(Graph<idNode> &graph, std::vector<User *> &users);
 
 void mainMenu(Graph<idNode> &graph, std::vector<User *> &users);
 void createNewUser(Graph<idNode> &graph, std::vector<User *> &users);
 
 void printtitle();
-void driverSetUp(User &driver, Graph<idNode> &graph,
-                 std::vector<User *> &users);
-
-void exit();
